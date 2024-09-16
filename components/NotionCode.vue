9fdc8fdec0ba204225c82b2fab099851a2c03524
ivansedov <ivansedov@fands.dev>
2024-09-16 03:32:53 +0200

    Fix code content disappear

--- components/NotionCode.vue
+++ components/NotionCode.vue
@@ -3,27 +3,29 @@
     :class="[
       'notion-code',
       langClass,
-    ]"><code :class="langClass">{{ properties.title[0][0] }}</code></pre>
+    ]"><code :class="langClass"><template v-for="(segment, segmentIndex) in properties.title" :key="segmentIndex">{{ segment[0] }}</template></code></pre>
 </template>
 
 <script>
-  import { Blockable, blockComputed, blockProps } from '../lib/blockable';
+import { defineComponent } from 'vue';
+import { Blockable, blockComputed, blockProps } from '../lib/blockable';
 
-  export default {
-    extends: Blockable,
-    props: { ...blockProps, overrideLang: String, overrideLangClass: String },
-
-    computed: {
-      ...blockComputed,
-      lang() {
-        return (
-          this.overrideLang ||
-          this.properties?.language?.[0]?.[0]?.toLowerCase()
-        );
-      },
-      langClass() {
-        return this.overrideLangClass || `language-${this.lang}`;
-      },
+export default defineComponent({
+  name: 'NotionCode',
+  extends: Blockable,
+  props: {
+    ...blockProps,
+    overrideLang: String,
+    overrideLangClass: String,
+  },
+  computed: {
+    ...blockComputed,
+    lang() {
+      return this.overrideLang || this.properties?.language?.[0]?.[0]?.toLowerCase() || 'plaintext';
     },
-  };
+    langClass() {
+      return this.overrideLangClass || `language-${this.lang}`;
+    },
+  }
+});
 </script>
