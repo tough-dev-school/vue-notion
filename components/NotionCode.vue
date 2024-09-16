002ba7f13b396b43ffd69e9a5c3079cee1fe494f
ivansedov <ivansedov@fands.dev>
2024-09-16 03:50:04 +0200

    Fix

--- components/NotionCode.vue
+++ components/NotionCode.vue
@@ -3,29 +3,27 @@
     :class="[
       'notion-code',
       langClass,
-    ]"><code :class="langClass"><template v-for="(segment, segmentIndex) in properties.title" :key="segmentIndex">{{ segment[0] }}</template></code></pre>
+    ]"><code :class="langClass"><template v-for="(segment, segmentIndex) in properties.title" :key="segmentIndex">{{ segment[0] }}</template></code>
+</pre>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import { Blockable, blockComputed, blockProps } from '../lib/blockable';
+  import { Blockable, blockComputed, blockProps } from '../lib/blockable';
 
-export default defineComponent({
-  name: 'NotionCode',
-  extends: Blockable,
-  props: {
-    ...blockProps,
-    overrideLang: String,
-    overrideLangClass: String,
-  },
-  computed: {
-    ...blockComputed,
-    lang() {
-      return this.overrideLang || this.properties?.language?.[0]?.[0]?.toLowerCase() || 'plaintext';
+  export default {
+    extends: Blockable,
+    props: { ...blockProps, overrideLang: String, overrideLangClass: String },
+    computed: {
+      ...blockComputed,
+      lang() {
+        return (
+          this.overrideLang ||
+          this.properties?.language?.[0]?.[0]?.toLowerCase()
+        );
+      },
+      langClass() {
+        return this.overrideLangClass || `language-${this.lang}`;
+      },
     },
-    langClass() {
-      return this.overrideLangClass || `language-${this.lang}`;
-    },
-  }
-});
+  };
 </script>
